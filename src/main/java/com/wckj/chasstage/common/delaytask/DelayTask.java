8abb6d0cf209658531241d9b38f7267c385b7b8c
zengruikai <1045000133@qq.com>
2024-05-07 00:17:34 +0800

    测试

--- src/main/java/com/wckj/chasstage/common/delaytask/DelayTask.java
+++ src/main/java/com/wckj/chasstage/common/delaytask/DelayTask.java
@@ -54,6 +54,9 @@ public class DelayTask extends TaskBase{
         }
     }
 
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
 
     public void outTime() {
         if(data.get("id")==null){
